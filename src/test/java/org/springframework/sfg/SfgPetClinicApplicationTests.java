f68635b0f8504b44eab58d23f747f082b896b4d0
Chiara Mameli <chiara.mameli@we-plus.eu>
2023-04-13 09:44:12 +0200

    Initialized

---
+++ src/test/java/org/springframework/sfg/SfgPetClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package org.springframework.sfg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SfgPetClinicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
